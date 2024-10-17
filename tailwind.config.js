984cac179169f4c4f82c249831d476246acf62fb
febe89 <fullstack9020@gmail.com>
2024-10-17 19:46:04 +0530

    form in tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    // color fonts border radius
+    extend: {
+      colors: {
+        twitter: '#1DA1F2'
+      }
+    }
+  },
+  plugins: []
+}
